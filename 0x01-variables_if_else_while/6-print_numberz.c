e7b0f0b059a42fb1e4b6b771f8ab8f076fd36a02
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-06 16:11:48 -0700

    tsk6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+	for (x = '0'; x <= 10; x++)
+	{
+		putchar('x');
+	}
+	return (0);
+}
+
