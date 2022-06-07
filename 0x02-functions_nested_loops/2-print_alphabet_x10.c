1555fa3dfdf947f5714ce6b0f0c3801752498d9c
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-07 14:25:58 -0700

    tsk1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * Return: No return value
+ */
+void print_alphabet_x10(void)
+{
+int x;
+char y;
+
+for (y = 'a'; y <= 'z'; y++)
+	{
+		putchar(y)
+	}
+for (x = 0; x = 10)
+	{
+		putchar(%d; x)
+	}
+return (0);
+}
