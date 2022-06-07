de634a835061b07460051bb21e36f2e578a1f0a8
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-07 16:16:03 -0700

    tsk2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+    print_alphabet_x10();
+
+    return (0);
+
+}
+
+vi 2-main.co
+clear
+\
+clear
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (counter = 0; counter < 10; counter++)
 	{
-		for  (c = 'a'; c < 'z'; c++)
+		for  (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
