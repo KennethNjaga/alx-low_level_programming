e8e0781acfa4b443b6c2ee15fa573dddd8289dc3
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-09 05:08:14 -0700

    tsk0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper -checks for uppercase character
+ * @c: character in uppercase
+ *
+ * Return: Always 1(Success) Otherwise 0 is returned
+ */
+int _isupper(int c)
+{
+if (c == 1)
+{
+_putchar(1);
+}
+else
+_putchar(0);
+}
