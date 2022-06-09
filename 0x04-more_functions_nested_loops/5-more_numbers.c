e409f9bb33eeaf55346040aac0e0378215f39959
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-09 10:26:31 -0700

    tsk5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
 void more_numbers(void)
 {
 
-int a, b;
+	int a, b;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 15; b++)
 		{
 			if (a >= 10)
-				_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0');
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
