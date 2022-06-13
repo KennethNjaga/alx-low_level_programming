1a361477523227b23794cd02315f757ce536b5a5
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-13 13:46:00 -0700

    tsk5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,32 +7,14 @@
  */
 void rev_string(char *s)
 {
-char *start_c, *end_c, c;
-int i, count;
-int length = 0;
+int c;
 
-for (i = 0; s[i]; i++)
-{
-length++;
+for (c = 0; s[c] != '\0'; c++)
+
+if (s[c] == '\0')
+
+c--;
+while
+(c > 0);
+
 }
-count = length;
-
-start_c = s;
-end_c = s;
-
-for (i = 0; i < count - 1; i++)
-{
-end_c++;
-}
-
-for (i = 0; i < count / 2; i++)
-{
-c = *end_c;
-*end_c = *start_c;
-*start_c = c;
-
-start_c++;
-end_c++;
-}
-}
-
