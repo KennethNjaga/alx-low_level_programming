a067c010b668e205226d140537f5b81343f2b6eb
KennethNjaga <101345055+KennethNjaga@users.noreply.github.com>
2022-06-21 15:07:10 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,55 +10,54 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
+int i, j;
 
-	int i, j;
+int count = 0;
 
-	int count = 0;
+char *str1, *str2;
 
-	char *str1, *str2;
+str1 = s;
 
-	str1 = s;
+str2 = accept;
 
-	str2 = accept;
+i = 0;
 
-	i = 0;
+while (str1[i] != '\0') /*Declaring WHILE *s */
 
-	while (str1[i] != '\0') /*Declaring WHILE *s */
+{
 
-	{
+j = 0;
 
-		j = 0;
+while (str2[j] != '\0') /*Declaring WHILE *accept*/
 
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+{
 
-		{
+if (str2[j] == str1[i]) /*Evaluate condition*/
 
-			if (str2[j] == str1[i]) /*Evaluate condition*/
+{
 
-			{
+count++; /*count number*/
 
-				count++; /*count number*/
+break;
 
-				break;
+}
 
-			}
+j++; /*add j+1*/
 
-			j++;    /*add j+1*/
+}
 
-		}
+if (s[i] != accept[j]) /*If aren't equals*/
 
-		if (s[i] != accept[j]) /*If aren't equals*/
+{
 
-		{
+break;
 
-			break;
+}
 
-		}
+i++; /*add x+1*/
 
-		i++; /*add x+1*/
+}
 
-	}
-
-	return (count); /*return the value of count*/
+return (count); /*return the value of count*/
 
 }
