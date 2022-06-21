0fc1acb9409402ddffc67ac3bf297110ba3d94f3
KennethNjaga <101345055+KennethNjaga@users.noreply.github.com>
2022-06-21 18:05:58 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- * @to: string
- * Return: void
+ *set_string - sets the value of a pointer to a char
+ *@s: content is to be copied
+ *@to: string
+ *Return: void
  */
 
 void set_string(char **s, char *to)
