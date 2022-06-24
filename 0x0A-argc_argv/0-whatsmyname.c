dd1e7805184a1b6a4cc5b1ad02571d5557ec7d95
KennethNjaga <kennethndirangu2@gmail.com>
2022-06-24 14:05:17 -0700

    tsk0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * program that prints its name
+ * followed by a new line
+ *
+ * Return: Always 0(Success)
+ */
+int main(int argc, char *argv[]);
