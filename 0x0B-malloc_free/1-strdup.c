c16000b0493a2f286e8c82965b473a30dca32309
KennethNjaga <101345055+KennethNjaga@users.noreply.github.com>
2022-06-29 14:59:08 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,58 +1,68 @@
 #include "main.h"
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * which contains a copy of the string given as a parameter
- * @str: string
- * Return: pointer
+ *_strlen - count array
+ *@s: array of elements
+ *Return: 1
  */
-char *_strdup(char *str)
-{
-char *dst;
-unsigned int size;
-if (str == 0)
-{
-return (NULL);
-}
-size = _strlen(str) + 1;
-dst = (char *) malloc(size *sizeof(char));
-if (dst == 0)
-{
-return (NULL);
-}
-_strcpy(dst, str);
-return (dst);
-}
 
-/**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
- */
 int _strlen(char *s)
 {
 unsigned int i;
+
 i = 0;
 while (s[i] != '\0') /*count character of string*/
 {
 i++;
 }
+
 return (i);
 }
 
 /**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
+ *_strcpy - copy arrays
+ *@src: array of elements
+ *@dest: dest array
+ *Return: dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
+
 while (src[i] != '\0')
 {
 dest[i] = src[i];
 i++;
 }
 dest[i] = '\0';
+
 return (dest);
+}
+
+/**
+ *_strdup - array for prints a string
+ *@str: array of elements
+ *Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+char *dst;
+unsigned int size;
+
+if (str == 0)
+{
+return (NULL);
+}
+
+size = _strlen(str) + 1;
+
+dst = (char *) malloc(size *sizeof(char));
+
+if (dst == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
 }
